ab7eea717ceee4d07dd1bd0a43d67e737e30d34f
ayk-dev <70164850+ayk-dev@users.noreply.github.com>
2021-03-13 11:16:11 -0600

    Add files via upload

---
+++ Even-Odd.js
@@ -0,0 +1,13 @@
+function evenOrOdd(num) {
+    number = Number(num);
+    if (number % 2 == 0) {
+        console.log('even')
+    }else{
+        console.log('odd')
+    }
+}
+
+evenOrOdd("5")
+evenOrOdd("2")
+evenOrOdd("0")
+evenOrOdd("11")
