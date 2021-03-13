e96892e99fd6b37d81df73f3b8a6668c39c75ca5
ayk-dev <70164850+ayk-dev@users.noreply.github.com>
2021-03-13 11:19:06 -0600

    Add files via upload

---
+++ Password-Guess.js
@@ -0,0 +1,8 @@
+function passwordGuess(pw) {
+    let correctPassword = 's3cr3t!P@ssw0rd';
+    let password = pw;
+    if (password == correctPassword)
+        console.log('Welcome');
+    else
+        console.log('Wrong password!') 
+}
