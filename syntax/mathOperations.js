26208786d5b8a02297b683d91c05045136026f0d
ayk-dev <70164850+ayk-dev@users.noreply.github.com>
2021-03-28 14:00:32 -0500

    Add files via upload

---
+++ syntax/mathOperations.js
@@ -0,0 +1,13 @@
+function mathOperations(numOne, numTwo, operator) {
+    let result;
+    switch (operator) {
+        case '+': result = numOne + numTwo; break;
+        case '-': result = numOne - numTwo; break;
+        case '/': result = numOne / numTwo; break;
+        case '*': result = numOne * numTwo; break;
+        case '%': result = numOne % numTwo; break;
+        case '**': result = numOne ** numTwo; break;
+    }
+
+    return result
+}
