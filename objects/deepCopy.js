d1be304a34b9ccb85a76352fa3f0418ed0fbde45
ayk-dev <70164850+ayk-dev@users.noreply.github.com>
2021-05-19 02:42:32 -0500

    Add files via upload

---
+++ objects/deepCopy.js
@@ -0,0 +1,10 @@
+function deepCopy(target) {
+    if (Array.isArray(target)) {
+        return target.map(deepCopy);
+    } else if (typeof target == 'object') {
+        return [...Object.defineProperties(target)].reduce((a, [k, v]) => Object.assign(a, {[k]: deepCopy(v)}), {});
+    } else {
+        return target;
+    }
+}
+
