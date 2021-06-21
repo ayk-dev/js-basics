07580799c99c69da137a1c8311d65143272ecc89
ayk-dev <70164850+ayk-dev@users.noreply.github.com>
2021-06-21 14:49:02 -0500

    Add files via upload

---
+++ dom/SumNumbers.js
@@ -0,0 +1,9 @@
+function calc() {
+    // TODO: sum = num1 + num2
+    const num1 = Number(document.getElementById('num1').value);
+    const num2 = Number(document.getElementById('num2').value);
+
+    const result = num1 + num2;
+
+    document.getElementById('sum').value = result;
+}
